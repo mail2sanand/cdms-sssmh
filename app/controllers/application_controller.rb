b16cc5e78cdb53b8e258089dee96df1a216e8967
Srinivasan, Anand <SrinivaA@advisory.com>
2019-11-30 03:16:32 +0530

    Date Calculation for the Next Month's Review Report Fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,18 +20,23 @@ class ApplicationController < ActionController::Base
     Date.today.year - age
   end
 
-  def calculate_next_month_village_date(village_date_order,report_for_month_of = nil)
+  def calculate_next_month_village_date(village_date_order,report_for_month_of = nil)    
     #Calculate and Format the Date
+
     year_month = Date.today
-    year = (year_month.month == 12 ? (year_month.year+1) : year_month.year)
 
-    # month_to_consider = (report_for_month_of ? report_for_month_of-1 : year_month.month)
-
-    # # In case the month is Dec, then default the next month to Jan and not month + 1
-    # month = (year_month.month == 12 ? 1 : (year_month.month + 1))
-    # month = (report_for_month_of == 12 ? 1 : report_for_month_of) if report_for_month_of
-
-    month = (report_for_month_of ? (report_for_month_of == 12 ? 1 : report_for_month_of) : (year_month.month == 12 ? 1 : (year_month.month + 1)) )
+    if report_for_month_of
+        month = report_for_month_of 
+        year = year_month.year
+    else
+      if year_month.month == 12
+        month = 1
+        year = year_month.year + 1
+      else
+        month = year_month.month + 1
+        year = year_month.year
+      end
+    end
 
     return Date.new(year,month,village_date_order).strftime("%d %b, %Y")
   end
